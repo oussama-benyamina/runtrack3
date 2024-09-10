59e2bfc822357b1e80fb1d976a0ff06427a7831c
oussama-benyamina <146718357+oussama-benyamina@users.noreply.github.com>
2024-09-10 19:49:32 +0200

    fain de project

--- jour01/job03/index.php
+++ jour01/job03/index.php
@@ -4,9 +4,10 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job 03</title>
-    <script src="script.js"></script>
+    
 </head>
 <body>
-    <!-- Le contenu de la page -->
+    
+    <script src="script.js"></script>
 </body>
 </html>
--- jour01/job04/index.php
+++ jour01/job04/index.php
@@ -4,9 +4,12 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Job 04</title>
-    <!-- Inclusion du fichier script.js -->
-    <script src="script.js"></script>
+    
 </head>
 <body>
+
+
+<!-- Inclusion du fichier script.js -->
+<script src="script.js"></script>
 </body>
 </html>
