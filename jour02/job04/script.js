24c6fb5a1577925a6f12e0213a6d91ee9ec4720e
oussama-benyamina <146718357+oussama-benyamina@users.noreply.github.com>
2024-09-15 17:19:13 +0200

    fin de job04

--- jour02/job04/script.js
+++ jour02/job04/script.js
@@ -1,9 +1,11 @@
+// Ajoute un écouteur d'événements pour les événements 'keypress' sur le document
 document.addEventListener('keypress', function(event) {
+    // Vérifie si la touche pressée est une lettre (a-z, insensible à la casse)
     if (event.key.match(/[a-z]/i)) {
+        // Sélectionne l'élément <textarea> avec l'ID 'keylogger'
         const keylogger = document.getElementById('keylogger');
+        
+        // Ajoute la lettre pressée au contenu du <textarea>
         keylogger.value += event.key;
-        if (document.activeElement === keylogger) {
-            keylogger.value += event.key;
-        }
     }
 });
