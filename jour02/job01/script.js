7cd45c01dbb010b2c5e1077cc210c6579fe175fd
oussama-benyamina <146718357+oussama-benyamina@users.noreply.github.com>
2024-09-15 16:49:53 +0200

    fain de job01

--- jour02/job01/script.js
+++ jour02/job01/script.js
@@ -1,6 +1,12 @@
+// Définition de la fonction citation
 function citation() {
+    // Récupère l'élément HTML avec l'ID 'citation'
     const citationElement = document.getElementById('citation');
+    
+    // Affiche le contenu texte de l'élément 'citation' dans la console du navigateur
     console.log(citationElement.textContent);
 }
 
+// Ajoute un écouteur d'événements au bouton avec l'ID 'button'
+// Lorsque le bouton est cliqué, la fonction citation() est appelée
 document.getElementById('button').addEventListener('click', citation);
