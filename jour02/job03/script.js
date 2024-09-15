207eba4eb820d6d2c50b0818ca7c6f72a7d6b10b
oussama-benyamina <146718357+oussama-benyamina@users.noreply.github.com>
2024-09-15 16:56:11 +0200

    fin de job03

--- jour02/job03/script.js
+++ jour02/job03/script.js
@@ -1,8 +1,15 @@
+// Initialise la variable count à 0
 let count = 0;
 
+// Définition de la fonction addone
 function addone() {
+    // Incrémente la variable count de 1
     count++;
+    
+    // Met à jour le contenu texte de l'élément avec l'ID 'compteur' pour afficher la nouvelle valeur de count
     document.getElementById('compteur').textContent = count;
 }
 
-document.getElementById('button').addEventListener('click', addone);    
+// Ajoute un écouteur d'événements au bouton avec l'ID 'button'
+// Lorsque le bouton est cliqué, la fonction addone() est appelée
+document.getElementById('button').addEventListener('click', addone);
