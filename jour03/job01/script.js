435171ad5da5a96d907e168a26e467beae3c81a1
oussama-benyamina <146718357+oussama-benyamina@users.noreply.github.com>
2024-09-15 18:21:47 +0200

    update job01

--- jour03/job01/script.js
+++ jour03/job01/script.js
@@ -1,9 +1,15 @@
+// Lorsque le document HTML est entièrement chargé et prêt à être manipulé
 $(document).ready(function() {
+
+    // Quand le bouton avec l'ID "showButton" est cliqué
     $("#showButton").click(function() {
+        // Affiche l'élément avec l'ID "quote" (qui peut être un paragraphe, une citation, etc.)
         $("#quote").show();
     });
 
+    // Quand le bouton avec l'ID "hideButton" est cliqué
     $("#hideButton").click(function() {
+        // Cache tout le contenu de la page en masquant l'élément 'body' entier
         $("body").hide();
     });
 });
